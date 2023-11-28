46b3ee69604c6132972c531f3c21f665b3648d3e
Matheus Passini <80512548+matheuspassini@users.noreply.github.com>
2023-11-28 13:37:23 -0300

    Update index.js

--- index.js
+++ index.js
@@ -1,42 +1,16 @@
-const form = document.querySelector('#form')
-const buttonSave = document.getElementById('save')
+const { income, expenses } = transactions.reduce(
+        (acc, transaction) => {
+            const amount = parseFloat(transaction.amount);
 
-async function getTransactions() {
-    try {
-        const response = await fetch('http://localhost:3000/transactions')
-        const data = await response.json()
-        return data
+            if (amount >= 0) {
+                acc.income += amount;
+            } else {
+                acc.expenses += amount;
+            }
 
-    } catch (error) {
-        alert(error)
-    }
-}
+            return acc;
+        },
+        { income: 0, expenses: 0 }
+    );
 
-document.addEventListener('DOMContentLoaded', () => {
-    getTransactions()
-})
-
-buttonSave.addEventListener('click', async (ev) => {
-    ev.preventDefault()
-
-    const transaction = {
-        name: document.querySelector('#name').value,
-        amount: document.querySelector('#amount').value,
-        id: document.querySelector('#id').value
-    }
-
-    try {
-        const posting = await fetch('http://localhost:3000/transactions', {
-            method: 'POST',
-            headers: {
-                'Content-Type': 'application/json'
-            },
-            body: JSON.stringify(transaction)
-        })
-
-        await posting.json()
-
-    } catch (error) {
-        alert(error)
-    }
-})
+    return { income, expenses };
